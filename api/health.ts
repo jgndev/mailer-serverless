6ea9e160754d9863424778320ade41a8c8ba3d35
Jeremy Novak <olepunchy@gmail.com>
2023-05-08 13:52:15 -0500

    Testing new function

---
+++ api/health.ts
@@ -0,0 +1,22 @@
+import {VercelRequest, VercelResponse} from "@vercel/node";
+
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,GET')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    return await fn(req, res)
+}
+
+const handler = async (request: VercelRequest, response: VercelResponse) => {
+    return response.status(200).send("OK");
+}
+
+module.exports = allowCors(handler);
